4787d3fa481a1bd8be9e169b8f4c71d2127a4b20
Ludwig.eth <ludwig.eth@gmail.com>
2024-08-01 01:20:44 +0200

    dkd

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html lang="en">
+		<Html className="bg-black" lang="en">
 			<Head>
 				<link rel="icon" type="image/svg+xml" href="/shibasvg.svg" />
 			</Head>
